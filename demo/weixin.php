a1b2056a08c702729336ec9125717aa06cc4bc89
tkerspp <674855453@qq.com>
2016-05-31 21:31:22 +0800

    zheshi wo de

---
+++ demo/weixin.php
@@ -0,0 +1,159 @@
+
+
+define("TOKEN", "weixin");
+
+$wechatObj = new wechatCallbackapiTest();
+if (!isset($_GET['echostr'])) {
+    $wechatObj->responseMsg();
+    
+}else{
+    $wechatObj->valid();
+}
+
+class wechatCallbackapiTest
+{
+    //验证签名
+    public function valid()
+    {
+        
+        $echoStr = $_GET["echostr"];
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr);
+        $tmpStr = implode($tmpArr);
+        $tmpStr = sha1($tmpStr);
+        if($tmpStr == $signature){
+            echo $echoStr;
+            exit;
+        }
+    }
+	
+    public function responseMsg()
+    {
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+        if (!empty($postStr)){
+            $this->logger("R ".$postStr);
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $RX_TYPE = trim($postObj->MsgType);
+		    $fromUsername = $postObj->FromUserName;
+            $toUsername = $postObj->ToUserName;
+            $keyword = trim($postObj->Content);
+            $time = time();
+            $textTpl = "<xml>
+                        <ToUserName><![CDATA[%s]]></ToUserName>
+                        <FromUserName><![CDATA[%s]]></FromUserName>
+                        <CreateTime>%s</CreateTime>
+                        <MsgType><![CDATA[%s]]></MsgType>
+                        <Content><![CDATA[%s]]></Content>
+                        <FuncFlag>0</FuncFlag>
+                        </xml>";
+            if($keyword == "徐琥" )
+            {
+                $msgType = "text";
+                $contentStr = "他好喜欢廖孟思";
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                echo $resultStr;
+                }
+             if($keyword == "廖孟思" )
+            {
+                $msgType = "text";
+                $contentStr = "她是个傻瓜";
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                echo $resultStr;
+                }
+            $result = "";
+            switch ($RX_TYPE)
+            {
+                case "event":
+                    $result = $this->receiveEvent($postObj);
+                    break;
+                case "text":
+                    $result = $this->receiveText($postObj);
+                    break;
+            }
+            $this->logger("T ".$result);
+            echo $result;
+        }else {
+            echo "";
+            exit;
+        }
+    }
+
+    private function receiveEvent($object)
+    {
+        switch ($object->Event)
+        {
+            case "subscribe":
+                $content = "欢迎关注方倍工作室 ";
+                break;
+        }
+        $result = $this->transmitText($object, $content);
+        return $result;
+    }
+
+    private function receiveText($object)
+    {
+        $keyword = trim($object->Content);
+        $url = "http://apix.sinaapp.com/weather/?appkey=".$object->ToUserName."&city=".urlencode($keyword); 
+        $output = file_get_contents($url);
+        $content = json_decode($output, true);
+
+        $result = $this->transmitNews($object, $content);
+        return $result;
+    }
+
+    private function transmitText($object, $content)
+    {
+        if (!isset($content) || empty($content)){
+            return "";
+        }
+        $textTpl = "<xml>
+<ToUserName><![CDATA[%s]]></ToUserName>
+<FromUserName><![CDATA[%s]]></FromUserName>
+<CreateTime>%s</CreateTime>
+<MsgType><![CDATA[text]]></MsgType>
+<Content><![CDATA[%s]]></Content>
+</xml>";
+        $result = sprintf($textTpl, $object->FromUserName, $object->ToUserName, time(), $content);
+        return $result;
+    }
+
+    private function transmitNews($object, $newsArray)
+    {
+        if(!is_array($newsArray)){
+            return "";
+        }
+        $itemTpl = "    <item>
+        <Title><![CDATA[%s]]></Title>
+        <Description><![CDATA[%s]]></Description>
+        <PicUrl><![CDATA[%s]]></PicUrl>
+        <Url><![CDATA[%s]]></Url>
+    </item>
+";
+        $item_str = "";
+        foreach ($newsArray as $item){
+            $item_str .= sprintf($itemTpl, $item['Title'], $item['Description'], $item['PicUrl'], $item['Url']);
+        }
+        $newsTpl = "<xml>
+<ToUserName><![CDATA[%s]]></ToUserName>
+<FromUserName><![CDATA[%s]]></FromUserName>
+<CreateTime>%s</CreateTime>
+<MsgType><![CDATA[news]]></MsgType>
+<Content><![CDATA[]]></Content>
+<ArticleCount>%s</ArticleCount>
+<Articles>
+$item_str</Articles>
+</xml>";
+
+        $result = sprintf($newsTpl, $object->FromUserName, $object->ToUserName, time(), count($newsArray));
+        return $result;
+    }
+
+    private function logger($log_content)
+    {
+      
+    }
+}
